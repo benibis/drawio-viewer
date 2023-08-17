9637471453b77e985e948fcffdc36adb6d42b914
benibis <67835168+benibis@users.noreply.github.com>
2023-08-17 16:33:24 +0200

    Update comparer.ps1 v2

--- comparer.ps1
+++ comparer.ps1
@@ -17,14 +17,14 @@ $files1 = Get-ChildItem -Path $path1 -Filter "*.drawio" -File -Recurse
 $files2 = Get-ChildItem -Path $path2 -Filter "*.drawio" -File -Recurse
 
 foreach ($file1 in $files1) {
-    $file2 = $files2 | Where-Object { $_.Name -eq $file1.Name }
+    $matchingFile = $files2 | Where-Object { $_.Name -eq $file1.Name -and $_.FullName -replace [regex]::Escape($path2), $path1 }
     
-    if ($file2) {
+    if ($matchingFile) {
         $content1 = Get-DrawioFileContent -filePath $file1.FullName
-        $content2 = Get-DrawioFileContent -filePath $file2.FullName
+        $content2 = Get-DrawioFileContent -filePath $matchingFile.FullName
         
         if ($content1 -ne $content2) {
-            Write-Host "Difference found in $($file1.FullName) and $($file2.FullName)"
+            Write-Host "Difference found in $($file1.FullName) and $($matchingFile.FullName)"
         }
     } else {
         Write-Host "File $($file1.FullName) does not exist in the second directory."
@@ -32,9 +32,9 @@ foreach ($file1 in $files1) {
 }
 
 foreach ($file2 in $files2) {
-    $file1 = $files1 | Where-Object { $_.Name -eq $file2.Name }
+    $matchingFile = $files1 | Where-Object { $_.Name -eq $file2.Name -and $_.FullName -replace [regex]::Escape($path1), $path2 }
     
-    if (!$file1) {
+    if (!$matchingFile) {
         Write-Host "File $($file2.FullName) does not exist in the first directory."
     }
 }
