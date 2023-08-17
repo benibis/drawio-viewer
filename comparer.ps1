eb1466d0e7e5a6cc8ce04f6cf5fa2446d98d4c38
benibis <67835168+benibis@users.noreply.github.com>
2023-08-17 17:04:01 +0200

    Update comparer.ps1

--- comparer.ps1
+++ comparer.ps1
@@ -3,18 +3,36 @@ param(
     [string]$path2
 )
 
-function Get-DrawioFileContent {
-    param(
-        [string]$filePath
-    )
-    
-    $content = Get-Content -Path $filePath -Raw
-    $xml = [xml]$content
-    return $xml
-}
-
 $timestamp = Get-Date -Format "yyyyMMddHHmmss"
 $outputFilePath = Join-Path $path2 ("$timestamp-drawio-compare.txt")
+
+function Compare-DrawioFiles {
+    param(
+        [string]$filePath1,
+        [string]$filePath2
+    )
+    
+    $xml1 = New-Object System.Xml.XmlDocument
+    $xml2 = New-Object System.Xml.XmlDocument
+
+    $xml1.Load($filePath1)
+    $xml2.Load($filePath2)
+
+    $differences = @()
+
+    $xml1.SelectNodes("//mxfile/*") | ForEach-Object {
+        $tag = $_
+        $name = $tag.Name
+        $value1 = $tag.Attributes["value"].Value
+        $value2 = $xml2.SelectSingleNode("//mxfile/$name").Attributes["value"].Value
+
+        if ($value1 -ne $value2) {
+            $differences += "$name: $value1 -> $value2"
+        }
+    }
+
+    return $differences
+}
 
 $files1 = Get-ChildItem -Path $path1 -Filter "*.drawio" -File -Recurse
 $files2 = Get-ChildItem -Path $path2 -Filter "*.drawio" -File -Recurse
@@ -25,30 +43,8 @@ foreach ($file1 in $files1) {
     $matchingFile = $files2 | Where-Object { $_.Name -eq $file1.Name -and $_.FullName -replace [regex]::Escape($path2), $path1 }
     
     if ($matchingFile) {
-        $xml1 = Get-DrawioFileContent -filePath $file1.FullName
-        $xml2 = Get-DrawioFileContent -filePath $matchingFile.FullName
-        
-        $xmlDiff = Compare-Object $xml1.InnerXml $xml2.InnerXml
-        
-        $differences = @()
-        
-        foreach ($diffItem in $xmlDiff) {
-            if ($diffItem.SideIndicator -eq '=>') {
-                $addedTag = (Select-Xml -Xml $xml2 -XPath $diffItem.InputObject).Node
-                $differences += "added $($addedTag.Name) $($addedTag.Attributes['value'].Value)"
-            } elseif ($diffItem.SideIndicator -eq '<=') {
-                $removedTag = (Select-Xml -Xml $xml1 -XPath $diffItem.InputObject).Node
-                $differences += "removed $($removedTag.Name) $($removedTag.Attributes['value'].Value)"
-            } else {
-                $tag = (Select-Xml -Xml $xml1 -XPath $diffItem.InputObject).Node
-                $value1 = $tag.Attributes['value'].Value
-                $value2 = (Select-Xml -Xml $xml2 -XPath $diffItem.InputObject).Node.Attributes['value'].Value
-                if ($value1 -ne $value2) {
-                    $differences += "$($tag.Name): $value1 -> $value2"
-                }
-            }
-        }
-        
+        $differences = Compare-DrawioFiles -filePath1 $file1.FullName -filePath2 $matchingFile.FullName
+
         if ($differences.Count -gt 0) {
             $outputLines += ("Differences found in $($file1.FullName) and $($matchingFile.FullName):" + $differences)
         }
@@ -61,10 +57,11 @@ foreach ($file2 in $files2) {
     $matchingFile = $files1 | Where-Object { $_.Name -eq $file2.Name -and $_.FullName -replace [regex]::Escape($path1), $path2 }
     
     if (!$matchingFile) {
-        $xml2 = Get-DrawioFileContent -filePath $file2.FullName
-        
-        $addedTags = $xml2.SelectNodes("//*[not(ancestor::*)]")
-        
+        $xml2 = New-Object System.Xml.XmlDocument
+        $xml2.Load($file2.FullName)
+
+        $addedTags = $xml2.SelectNodes("//mxfile/*")
+
         foreach ($tag in $addedTags) {
             $outputLines += ("added $($tag.Name) $($tag.Attributes['value'].Value)")
         }
